69ce7d749aa57b5788ac74f223a44a01d90f66ce
abdo hassan <abdocoder0@gmail.com>
2020-09-13 02:16:21 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,17 +1,122 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Slider from './components/Slider';
 import SidebarItem from './components/SidebarItem';
 import './App.css';
 
+const DEFAULT_OPTIONS = [
+  {
+    name: 'Brightness',
+    property: 'brightness',
+    value: 100,
+    range: {
+      min: 0,
+      max: 200,
+    },
+    unit: '%',
+  },
+  {
+    name: 'Contrast',
+    property: 'contrast',
+    value: 100,
+    range: {
+      min: 0,
+      max: 200,
+    },
+    unit: '%',
+  },
+  {
+    name: 'Saturation',
+    property: 'saturate',
+    value: 100,
+    range: {
+      min: 0,
+      max: 200,
+    },
+    unit: '%',
+  },
+  {
+    name: 'Grayscale',
+    property: 'grayscale',
+    value: 0,
+    range: {
+      min: 0,
+      max: 100,
+    },
+    unit: '%',
+  },
+  {
+    name: 'Sepia',
+    property: 'sepia',
+    value: 0,
+    range: {
+      min: 0,
+      max: 100,
+    },
+    unit: '%',
+  },
+  {
+    name: 'Hue Rotate',
+    property: 'hue-rotate',
+    value: 0,
+    range: {
+      min: 0,
+      max: 360,
+    },
+    unit: 'deg',
+  },
+  {
+    name: 'Blur',
+    property: 'blur',
+    value: 0,
+    range: {
+      min: 0,
+      max: 20,
+    },
+    unit: 'px',
+  },
+];
+
 function App() {
+  const [selectedOptionIndex, setSelectedOptionIndex] = useState(0);
+  const [options, setOptions] = useState(DEFAULT_OPTIONS);
+  const selectedOption = options[selectedOptionIndex];
+
+  const handleChange = ({ target }) => {
+    setOptions((prevOptions) => {
+      return prevOptions.map((option, index) => {
+        if (index !== selectedOptionIndex) return option;
+        return { ...option, value: target.value };
+      });
+    });
+  };
+
+  const getImageStyle = () => {
+    const filters = options.map((option) => {
+      return `${option.property}(${option.value}${option.unit})`;
+    });
+
+    return { filter: filters.join(' ') };
+  };
+
   return (
     <div className='container'>
-      <div className='main-image'></div>
+      <div className='main-image' style={getImageStyle()}></div>
       <div className='sidebar'>
-        <SidebarItem />
-        <SidebarItem />
+        {options.map((option, index) => (
+          <SidebarItem
+            key={index}
+            name={option.name}
+            active={index === selectedOptionIndex}
+            handleClick={() => setSelectedOptionIndex(index)}
+          />
+        ))}
       </div>
-      <Slider />
+      <Slider
+        min={selectedOption.range.min}
+        max={selectedOption.range.max}
+        value={selectedOption.value}
+        handleChange={handleChange}
+      />
     </div>
   );
 }
--- src/components/SidebarItem.js
+++ src/components/SidebarItem.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const SidebarItem = () => {
-  return <button className='sidebar-item active'>Sidebar Item</button>;
+const SidebarItem = ({ name, active, handleClick }) => {
+  return (
+    <button
+      className={`sidebar-item ${active ? 'active' : ''}`}
+      onClick={handleClick}
+    >
+      {name}
+    </button>
+  );
 };
 
 export default SidebarItem;
